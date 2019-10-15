71c7b0558625aa9fae46c348cc1bb3c2ed529e9d
Manuel Manelli <manuelmanelli@icloud.com>
2019-10-15 17:02:09 +0200

    Login convertita per usare task asincrono e metodo POST

--- app/src/main/java/com/example/gestioneRicevimenti/LoginActivity.java
+++ app/src/main/java/com/example/gestioneRicevimenti/LoginActivity.java
@@ -88,60 +88,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         passw = etpsw.getText().toString().trim();
         if(receiver.CheckConnection(LoginActivity.this)) {
 
-            //LoginRequest loginTask = new LoginRequest();
-            //loginTask.execute();
-
-            HttpURLConnection client = null;
-            JSONObject json_data = null;
-            String data = "matricola=" + matricola + "&password=" + passw;
-            try {
-                URL url = new URL("http://pmapp.altervista.org/login.php?" + data);
-                client = (HttpURLConnection) url.openConnection();
-                client.setRequestMethod("GET");
-                client.setDoInput(true);
-                InputStream in = client.getInputStream();
-                String json_string = ReadResponse.readStream(in);
-                json_data = ReadResponse.convert2JSON(json_string);
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (client != null) {
-                    client.disconnect();
-                }
-            }
-
-            if (json_data != null) {
-                Iterator<String> iter = json_data.keys();
-                while (iter.hasNext()) {
-                    String key = iter.next();
-                    try {
-                        JSONObject value = json_data.getJSONObject(key);
-                        String file = getPackageName() + "login_file";
-                        SharedPreferences sp = getSharedPreferences(file, Context.MODE_PRIVATE);
-                        SharedPreferences.Editor e = sp.edit();
-                        e.putString("matricola", matricola);
-                        e.putString("password", passw);
-                        e.putString("id_utente", value.getString("id"));
-                        e.putString("tipo_utente", value.getString("tipo"));
-                        e.apply();
-                        switch (sp.getString("tipo_utente", "")) {
-                            case "s":
-                                Intent i = new Intent(LoginActivity.this, StudentHomePageActivity.class);
-                                startActivity(i);
-                                break;
-                            case "p":
-                                Intent j = new Intent(LoginActivity.this, ProfHomePageActivity.class);
-                                startActivity(j);
-                                break;
-                            default:
-                        }
-                    } catch (JSONException e) {
-                        // Something went wrong!
-                    }
-                }
-            } else {
-                Toast.makeText(this, "LoginActivity fallito, matricola o/e password errate!", Toast.LENGTH_SHORT).show();
-            }
+            LoginRequest loginTask = new LoginRequest();
+            loginTask.execute();
         }
     }
 
@@ -153,7 +101,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             JSONObject json_data = null;
             if((!TextUtils.isEmpty(matricola)) && (!TextUtils.isEmpty(passw))){
                 try {
-                    URL url = new URL("http://pmapp.altervista.org/login_post.php");
+                    URL url = new URL("http://pmapp.altervista.org/login.php");
                     client = (HttpURLConnection) url.openConnection();
                     client.setRequestMethod("POST");
                     client.setDoOutput(true);
